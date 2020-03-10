119c4844a2d848ff462e5deb5dae1b8bdad2f6c5
alvaro <sanhueza.alvaro19@gmail.com>
2020-03-09 23:18:39 -0300

    Upload all project

---
+++ app/src/main/java/com/example/arapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.arapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+
+            ModelRenderable.builder()
+                    .setSource(MainActivity.this, Uri.parse("arcticfox.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(throwable -> {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage(throwable.getMessage())
+                                .show();
+                        return null;
+                    });
+        });
+
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+
+        AnchorNode anchorNode = new AnchorNode(anchor);
+
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+
+        transformableNode.setRenderable(modelRenderable);
+
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+        transformableNode.select();
+
+    }
+}
